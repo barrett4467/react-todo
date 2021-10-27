0a5dc6443a625750e02572a6a8b73ac4e7d60c73
barrett4467 <barrett4467@gmail.com>
2021-10-27 12:00:11 -0600

    Adding styles

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -61,14 +61,17 @@ state = {
     };
     render() {
         return (
-            <div>
-                <Header />
-                <InputTodo addTodoProps= {this.addTodoItem} />
-                <TodosList
-                    todos={this.state.todos}
-                    handleChangeProps={this.handleChange}
-                    deleteTodoProps={this.delTodo}
-                />
+            <div className="container">
+                <div className="inner">
+                    <Header />
+                    <InputTodo addTodoProps= {this.addTodoItem} />
+                    <TodosList
+                        todos={this.state.todos}
+                        handleChangeProps={this.handleChange}
+                        deleteTodoProps={this.delTodo}
+                    />
+
+                </div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
 //component file
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from "./components/TodoContainer";
+
+//Stylesheet
+import "./App.css";
+
 
 ReactDOM.render(
     <React.StrictMode>
